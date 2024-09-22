39641e50eff9cdd7cc44ae94c1811925695f2f84
aibeknet <aaskarov0690.kg@gmail.com>
2024-09-22 22:58:59 +0600

    Created Cell component.

---
+++ src/components/Cell.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface CellProps {
+    index: number;
+    onClick: (index: number) => void;
+    hasItem: boolean;
+    clicked: boolean;
+}
+
+const Cell: React.FC<CellProps> = ({index, onClick, hasItem, clicked}) => {
+    const handleClick = () => {
+        if (!clicked) {
+            onClick(index);
+        }
+    };
+
+    return (
+        <div onClick={handleClick} className={`cell ${clicked ? 'clicked' : ''}`}>
+            {clicked && hasItem ? 'o' : ''}
+        </div>
+    );
+};
+
+export default Cell;
